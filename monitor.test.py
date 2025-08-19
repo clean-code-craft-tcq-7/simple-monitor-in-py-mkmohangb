910e9ff1dae3d1fe89e0919909a69c3c232f32b9
Manoj Mohan <haimanojkumar@gmail.com>
2025-08-19 16:22:40 +0530

    add mock for displayAlert

--- monitor.test.py
+++ monitor.test.py
@@ -1,25 +1,38 @@
 import unittest
+import unittest.mock as mock
 from monitor import vitals_ok, alert_not_in_range
 
 
 class MonitorTest(unittest.TestCase):
-    def test_no_alert_when_in_range(self):
+    @mock.patch('monitor.displayVitalsAlert')
+    def test_no_alert_when_in_range(self, mock_alert):
         self.assertTrue(alert_not_in_range("Normal", 98, 95, 102))
+        mock_alert.assert_not_called()
 
-    def test_alert_when_not_in_range(self):
+    @mock.patch('monitor.displayVitalsAlert')
+    def test_alert_when_not_in_range(self, mock_alert):
         self.assertFalse(alert_not_in_range("Temperature out of Range", 94, 95, 102))
+        mock_alert.assert_called_once_with("Temperature out of Range")
     
-    def test_temperature_ok(self):
+    @mock.patch('monitor.displayVitalsAlert')
+    def test_temperature_ok(self, mock_alert):
         self.assertTrue(vitals_ok(98, 75, 95))
-    
-    def test_pulse_rate_critical(self):
-        self.assertFalse(vitals_ok(98, 40, 95))
+        mock_alert.assert_not_called()
 
-    def test_spo2_critical(self):
+    @mock.patch('monitor.displayVitalsAlert')
+    def test_pulse_rate_critical(self, mock_alert):
+        self.assertFalse(vitals_ok(98, 40, 95))
+        mock_alert.assert_called_once_with('Pulse Rate out of range!')
+
+    @mock.patch('monitor.displayVitalsAlert')
+    def test_spo2_critical(self, mock_alert):
         self.assertFalse(vitals_ok(98, 75, 85))
+        mock_alert.assert_called_once_with('Oxygen Saturation out of range!')
     
-    def test_all_vitals_ok(self):
+    @mock.patch('monitor.displayVitalsAlert')
+    def test_all_vitals_ok(self, mock_alert):
         self.assertTrue(vitals_ok(98, 75, 95))
+        mock_alert.assert_not_called()
 
 if __name__ == '__main__':
   unittest.main()

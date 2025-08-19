e0f3807b73297a6026c80970bc0fa6ead1e7a61c
Manoj Mohan <haimanojkumar@gmail.com>
2025-08-19 16:12:47 +0530

    add tests

--- monitor.py
+++ monitor.py
@@ -2,36 +2,34 @@
 from time import sleep
 import sys
 
+def displayVitalsAlert(message):
+  print(message)
+  for i in range(6):
+    print('\r* ', end='')
+    sys.stdout.flush()
+    sleep(1)
+    print('\r *', end='')
+    sys.stdout.flush()
+    sleep(1)
 
-def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
+def alert_not_in_range(message, value, min_value, max_value):
+  if not is_in_range(value, min_value, max_value):
+    displayVitalsAlert(message)
     return False
   return True
+
+def is_in_range(value, min_value, max_value):
+  return min_value <= value <= max_value
+
+def isTemperatureOk(temperature):
+  return alert_not_in_range('Temperature out of range', temperature, 95, 102)
+
+def isPulseRateOk(pulseRate):
+  return alert_not_in_range('Pulse Rate out of range!', pulseRate, 60, 100)
+
+def isSpo2Ok(spo2):
+  return alert_not_in_range('Oxygen Saturation out of range!', spo2, 90, 100)
+
+def vitals_ok(temperature, pulseRate, spo2):
+  return isTemperatureOk(temperature) and \
+         isPulseRateOk(pulseRate) and isSpo2Ok(spo2)
--- monitor.test.py
+++ monitor.test.py
@@ -1,12 +1,25 @@
 import unittest
-from monitor import vitals_ok
+from monitor import vitals_ok, alert_not_in_range
 
 
 class MonitorTest(unittest.TestCase):
-    def test_not_ok_when_any_vital_out_of_range(self):
-        self.assertFalse(vitals_ok(99, 102, 70))
-        self.assertTrue(vitals_ok(98.1, 70, 98))
+    def test_no_alert_when_in_range(self):
+        self.assertTrue(alert_not_in_range("Normal", 98, 95, 102))
 
+    def test_alert_when_not_in_range(self):
+        self.assertFalse(alert_not_in_range("Temperature out of Range", 94, 95, 102))
+    
+    def test_temperature_ok(self):
+        self.assertTrue(vitals_ok(98, 75, 95))
+    
+    def test_pulse_rate_critical(self):
+        self.assertFalse(vitals_ok(98, 40, 95))
+
+    def test_spo2_critical(self):
+        self.assertFalse(vitals_ok(98, 75, 85))
+    
+    def test_all_vitals_ok(self):
+        self.assertTrue(vitals_ok(98, 75, 95))
 
 if __name__ == '__main__':
   unittest.main()
